b118982a878c01d4e250ed2c8fcca172526f08fc
gameboy87 <vivek.ananth87@gmail.com>
2017-11-05 12:59:57 +0530

    changing css loader config for webpack 2 version

--- webpack.config.js
+++ webpack.config.js
@@ -28,8 +28,7 @@ module.exports = {
             },
             {
                 test:/\.css$/,
-                loader:'style-loader!css-loader',
-                //use:['css-loader']
+                use:['style-loader', 'css-loader']
             },
             {
                 test: /\.html$/,
